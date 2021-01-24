0170e0673cdc46dfa419c77ab57286b32c5940e0
Ann Duong <annduong@Anns-Air.fios-router.home>
2021-01-24 01:06:36 -0500

    added color feature

--- index.js
+++ index.js
@@ -1,64 +1,52 @@
-let player = document.getElementById("player");
-let opponent = document.getElementById("opponent");
+let square = document.getElementById("square");
 let game = document.getElementById("opponent");
 
-opponent.addEventListener("click", function(e){
-  opponent.parentNode.removeChild(opponent)
+square.addEventListener("click", function(){
+  square.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
 })
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+    moveLeft();
   }else if(e.key === "ArrowRight"){
-    moveDodgerRight()
+    moveRight()
   }
   else if(e.key === "ArrowDown"){
-    moveDodgerDown()
+    moveDown()
   }
   else if(e.key === "ArrowUp"){
-    moveDodgerUp()
+    moveUp()
   }
-  checkCollision()
 });
 
-function moveDodgerLeft() {
-  var leftPixels = player.style.left.replace("px", "");
+function moveLeft() {
+  var leftPixels = square.style.left.replace("px", "");
   var left = parseInt(leftPixels);
   if (left > 0) {
-    player.style.left = `${left - 5}px`;
+    square.style.left = `${left - 5}px`;
   }
 }
 
-function moveDodgerRight() {
-  var leftPixels = player.style.left.replace("px", "");
+function moveRight() {
+  var leftPixels = square.style.left.replace("px", "");
   var left = parseInt(leftPixels);
   if (left < 300) {
-    player.style.left = `${left + 5}px`;
+    square.style.left = `${left + 5}px`;
   }
 }
 
-function moveDodgerDown() {
-  var topPixels = player.style.top.replace("px", "");
+function moveDown() {
+  var topPixels = square.style.top.replace("px", "");
   var top = parseInt(topPixels);
   if (top < 300) {
-    player.style.top = `${top + 5}px`;
+    square.style.top = `${top + 5}px`;
   }
 }
 
-function moveDodgerUp(){
-  var topPixels = player.style.top.replace("px", "");
+function moveUp(){
+  var topPixels = square.style.top.replace("px", "");
   var top = parseInt(topPixels);
   if (top > 0) {
-    player.style.top = `${top - 5}px`;
-  }
-}
-
-function checkCollision() {
-  let left = parseInt(player.style.left.replace("px", ""));
-  let top = parseInt(player.style.top.replace("px", ""));
-  if(left >= 100 && left <=200 && top >= 50 && top <= 150) {
-    if(opponent.parentNode){
-      opponent.parentNode.removeChild(opponent)
-    }
+    square.style.top = `${top - 5}px`;
   }
 }
