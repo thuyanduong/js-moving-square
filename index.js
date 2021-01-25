a261c36e5a72d58ce69e4f45438d131b766288a4
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2021-01-24 21:13:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-let square=document.getElementById("square"),button=document.querySelector("button"),game=document.getElementById("game"),opponent=document.createElement("div");function checkCollision(){let e=parseInt(square.style.left.replace("px","")),t=parseInt(square.style.top.replace("px",""));if(opponent.parentNode){let o=parseInt(opponent.style.left.replace("px","")),n=parseInt(opponent.style.top.replace("px",""));e>=o-50&&e<=o+50&&t<=n+50&&t>=n-50&&opponent.parentNode.removeChild(opponent)}}function createSquare(){opponent.parentNode&&opponent.parentNode.removeChild(opponent),opponent.style.top=Math.floor(300*Math.random())+"px",opponent.style.left=Math.floor(300*Math.random())+"px",game.appendChild(opponent)}function moveLeft(){var e=square.style.left.replace("px",""),t=parseInt(e);t>0&&(square.style.left=`${t-10}px`)}function moveRight(){var e=square.style.left.replace("px",""),t=parseInt(e);t<300&&(square.style.left=`${t+10}px`)}function moveDown(){var e=square.style.top.replace("px",""),t=parseInt(e);t<300&&(square.style.top=`${t+10}px`)}function moveUp(){var e=square.style.top.replace("px",""),t=parseInt(e);t>0&&(square.style.top=`${t-10}px`)}opponent.style.backgroundColor="white",opponent.style.position="absolute",opponent.style.width="50px",opponent.style.height="50px",button.addEventListener("click",function(){createSquare()}),square.addEventListener("click",function(){square.style.backgroundColor="#"+Math.floor(16777215*Math.random()).toString(16)}),document.addEventListener("keydown",function(e){"ArrowLeft"===e.key?moveLeft():"ArrowRight"===e.key?moveRight():"ArrowDown"===e.key?moveDown():"ArrowUp"===e.key&&moveUp(),checkCollision()});
+let square = document.getElementById("square");
+let button = document.querySelector("button");
+let game = document.getElementById("game");
+let opponent = document.createElement("div");
+opponent.style.backgroundColor = "white";
+opponent.style.position = "absolute";
+opponent.style.width = "50px";
+opponent.style.height = "50px";
+
+button.addEventListener("click", function(){
+  createSquare()
+})
+
+function checkCollision() {
+  let left = parseInt(square.style.left.replace("px", ""));
+  let top = parseInt(square.style.top.replace("px", ""));
+  if(opponent.parentNode){
+    let opponentLeft = parseInt(opponent.style.left.replace("px", ""));
+    let opponentTop = parseInt(opponent.style.top.replace("px", ""));
+    if(left >= opponentLeft - 50 && left<= opponentLeft + 50 && top <= opponentTop + 50 && top >= opponentTop - 50) {
+      opponent.parentNode.removeChild(opponent)
+    }
+  }
+  
+}
+
+function createSquare(){
+  if(opponent.parentNode){
+    opponent.parentNode.removeChild(opponent)
+  }
+  opponent.style.top = Math.floor(Math.random() * 300) + "px"
+  opponent.style.left = Math.floor(Math.random() * 300) + "px"
+  game.appendChild(opponent)
+}
+
+square.addEventListener("click", function(){
+  square.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+})
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveLeft();
+  }else if(e.key === "ArrowRight"){
+    moveRight()
+  }
+  else if(e.key === "ArrowDown"){
+    moveDown()
+  }
+  else if(e.key === "ArrowUp"){
+    moveUp()
+  }
+  checkCollision();
+});
+
+function moveLeft() {
+  var leftPixels = square.style.left.replace("px", "");
+  var left = parseInt(leftPixels);
+  if (left > 0) {
+    square.style.left = `${left - 10}px`;
+  }
+}
+
+function moveRight() {
+  var leftPixels = square.style.left.replace("px", "");
+  var left = parseInt(leftPixels);
+  if (left < 300) {
+    square.style.left = `${left + 10}px`;
+  }
+}
+
+function moveDown() {
+  var topPixels = square.style.top.replace("px", "");
+  var top = parseInt(topPixels);
+  if (top < 300) {
+    square.style.top = `${top + 10}px`;
+  }
+}
+
+function moveUp(){
+  var topPixels = square.style.top.replace("px", "");
+  var top = parseInt(topPixels);
+  if (top > 0) {
+    square.style.top = `${top - 10}px`;
+  }
+}
